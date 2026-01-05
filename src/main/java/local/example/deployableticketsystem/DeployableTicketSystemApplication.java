7f5c25eaffb282545c966c0be2a5889bcfadf2be
pluvivanto <140719273+pluvivanto@users.noreply.github.com>
2026-01-05 18:38:29 +0900

    initial commit

---
+++ src/main/java/local/example/deployableticketsystem/DeployableTicketSystemApplication.java
@@ -0,0 +1,13 @@
+package local.example.deployableticketsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployableTicketSystemApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DeployableTicketSystemApplication.class, args);
+  }
+
+}
---
+++ src/test/java/local/example/deployableticketsystem/DeployableTicketSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package local.example.deployableticketsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployableTicketSystemApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
