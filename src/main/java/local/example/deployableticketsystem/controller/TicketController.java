8ba4dad467df118ef274331fbb87f2961df293c8
pluvivanto <140719273+pluvivanto@users.noreply.github.com>
2026-01-06 09:13:13 +0900

    add TicketController and TicketService

---
+++ src/main/java/local/example/deployableticketsystem/controller/TicketController.java
@@ -0,0 +1,16 @@
+package local.example.deployableticketsystem.controller;
+
+import local.example.deployableticketsystem.service.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TicketController {
+
+  private final TicketService ticketService;
+
+  @Autowired
+  public TicketController(TicketService ticketService) {
+    this.ticketService = ticketService;
+  }
+}
---
+++ src/main/java/local/example/deployableticketsystem/service/TicketService.java
@@ -0,0 +1,43 @@
+package local.example.deployableticketsystem.service;
+
+import java.util.UUID;
+import local.example.deployableticketsystem.entity.Reservation;
+import local.example.deployableticketsystem.entity.ReservationStatusEnum;
+import local.example.deployableticketsystem.entity.Ticket;
+import local.example.deployableticketsystem.repository.ReservationRepository;
+import local.example.deployableticketsystem.repository.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TicketService {
+
+  private final TicketRepository ticketRepository;
+  private final ReservationRepository reservationRepository;
+
+  @Autowired
+  public TicketService(TicketRepository ticketRepository,
+      ReservationRepository reservationRepository) {
+    this.ticketRepository = ticketRepository;
+    this.reservationRepository = reservationRepository;
+  }
+
+  public Boolean reserve(UUID ticketId, String userId) {
+    Ticket ticket = ticketRepository.findById(ticketId)
+        .orElseThrow(() -> new IllegalArgumentException(
+            "Ticket not found"));
+    Integer remainingQuantity = ticket.getRemainingQuantity();
+    if (remainingQuantity <= 0) {
+      System.err.println("There isn't any remaining ticket for " + ticketId);
+      return false;
+    }
+    Reservation reservation = new Reservation();
+    reservation.setUserId(userId);
+    reservation.setTicketId(ticket);
+    reservation.setStatus(ReservationStatusEnum.PENDING);
+    reservationRepository.save(reservation);
+    ticket.setRemainingQuantity(remainingQuantity - 1);
+    ticketRepository.save(ticket);
+    return true;
+  }
+}
